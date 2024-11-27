8e048e00e5a285597cf49cc55bff7228d68b0b07
DharmaWarrior <97218268+DharmaWarrior@users.noreply.github.com>
2024-11-28 00:31:36 +0530

    changed

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "https://askfinance.onrender.com/", // Replace with your actual Render URL
+    API_BASE_URL: "https://askfinance.onrender.com", // Replace with your actual Render URL
 };
 
 export default config;
