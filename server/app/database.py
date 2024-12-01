613ac9f5ff76e060f7135cf5aab5745f1c098f16
DharmaWarrior <97218268+DharmaWarrior@users.noreply.github.com>
2024-12-01 15:31:45 +0530

    updated packages

--- server/app/database.py
+++ server/app/database.py
@@ -2,13 +2,16 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
+
 # MongoDB Atlas connection string
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI, tls=True)
+client = AsyncIOMotorClient(MONGO_URI, tls=True)
 db = client['qa_app']  # Database name
+
 
 # Helper to convert MongoDB object to JSON
 def to_json(data):
